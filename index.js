6aa91129b791132dd2843346455054e79ee7367f
pauline-wim <linette.wim@hotmail.com>
2022-03-29 15:54:15 +0200

    Create student & GET student by id (w/ full address)

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.get("/", (req, res) => {
   res.send("Hello World !");
 });
 
+// Get address list
 app.get("/address", async (req, res) => {
   let address;
   try {
@@ -38,6 +39,37 @@ app.get("/address", async (req, res) => {
   res.json(address);
 });
 
+// Get student list
+app.get("/student", async (req, res) => {
+  let student;
+  try {
+    student = await Student.find().select("-__v");
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      message: "CANNOT find this student in the database",
+    });
+  }
+  res.json(student);
+});
+
+// Get student by id
+app.get("/student/:studentId", async (req, res) => {
+  let student;
+  try {
+    student = await Student.findById(req.params.studentId)
+      .select("-__v")
+      .populate("address");
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      message: "CANNOT find this student in the database",
+    });
+  }
+  res.json(student);
+});
+
+// Create address
 app.post("/address", async (req, res) => {
   let address;
   try {
@@ -50,6 +82,21 @@ app.post("/address", async (req, res) => {
   res.status(201).json({
     message: `Address ${req.body.streetNumber} ${req.body.streetName} created`,
     address,
+  });
+});
+
+// Create student
+app.post("/student", async (req, res) => {
+  let student;
+  try {
+    student = await Student.create(req.body);
+  } catch (err) {
+    return res.status(401).json({
+      message: `ERROR: ${err}`,
+    });
+  }
+  res.status(201).json({
+    message: `Student ${req.body.firstName} ${req.body.surname} created`,
   });
 });
 
--- models/studentModel.js
+++ models/studentModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const StudentSchema = mongoose.Schema({
   firstName: String,
   surname: String,
-  address: [{ type: mongoose.Types.ObjectId, ref: "Address" }],
+  address: { type: mongoose.Types.ObjectId, ref: "Address" },
 });
 
 const Student = mongoose.model("Student", StudentSchema);
