d39e19bbba69f6ece8abc53497e8e151cf5e99bd
pauline-wim <linette.wim@hotmail.com>
2022-03-29 14:51:34 +0200

    Created Student model

---
+++ models/studentModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const StudentSchema = mongoose.Schema({
+  firstName: String,
+  surname: String,
+  address: [{ type: mongoose.Types.ObjectId, ref: "Address" }],
+});
+
+const Student = mongoose.model("Student", StudentSchema);
+
+module.exports = Student;
