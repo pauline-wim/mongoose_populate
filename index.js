b989168a6f6545fc8eb76d737eb3cbc327288fcd
pauline-wim <linette.wim@hotmail.com>
2022-03-29 15:14:59 +0200

    Routes: POST address & GET all adresses

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const dotenv = require("dotenv");
 dotenv.config({
   path: "./config.env",
 });
+// Models
+const Student = require("./models/studentModel");
+const Address = require("./models/addressModel");
 
 // Connexion to MongoDB
 mongoose
@@ -20,6 +23,34 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World !");
+});
+
+app.get("/address", async (req, res) => {
+  let address;
+  try {
+    address = await Address.find().select("-__v");
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      message: "CANNOT find this address in the database",
+    });
+  }
+  res.json(address);
+});
+
+app.post("/address", async (req, res) => {
+  let address;
+  try {
+    address = await Address.create(req.body);
+  } catch (err) {
+    return res.status(401).json({
+      message: `ERROR: ${err}`,
+    });
+  }
+  res.status(201).json({
+    message: `Address ${req.body.streetNumber} ${req.body.streetName} created`,
+    address,
+  });
 });
 
 // ERROR
